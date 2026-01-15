3ba21aa4540d7a5097b2f2804e72419508bdc02f
Deepak <davdeepak1676@gmail.com>
2026-01-16 01:42:48 +0530

    initial push

---
+++ src/main/java/com/dev/Food_Club/FoodClubApplication.java
@@ -0,0 +1,13 @@
+package com.dev.Food_Club;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodClubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodClubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/Food_Club/FoodClubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.Food_Club;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodClubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
