81fe546004d1d730f76c07ba59ff94dc34e5dcda
themanoj1 <manojkumarsaw949@gmail.com>
2026-02-07 05:12:52 +0530

    created v2 for add-cart api

--- src/main/java/com/dev/Food_Club/controller/SelectedMenuController.java
+++ src/main/java/com/dev/Food_Club/controller/SelectedMenuController.java
@@ -3,15 +3,13 @@ package com.dev.Food_Club.Controller;
 import com.dev.Food_Club.DTO.SelectedMenuDto;
 import com.dev.Food_Club.Service.SelectedMenuService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
 
 @RestController
+@RequestMapping(path="/food_club/api/")
 
 public class SelectedMenuController {
 
@@ -27,4 +25,12 @@ public class SelectedMenuController {
 
     }
 
+
+    @PostMapping("v2/add-cart/{restaurantId}")
+    public Map<String,Object> AddToCart(@PathVariable Long restaurantId,
+                                               @RequestBody List<SelectedMenuDto> selectedMenuRequest){
+
+        return selectedMenuService.saveMenuItem(restaurantId,selectedMenuRequest);
+
+    }
 }
